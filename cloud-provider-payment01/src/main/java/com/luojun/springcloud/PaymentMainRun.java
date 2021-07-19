6531130484dc888f40c89ce062d7b34acd9e59f3
luojun <1195458334@qq.com>
2021-07-20 00:20:54 +0800

    解决依赖

---
+++ cloud-provider-payment01/src/main/java/com/luojun/springcloud/PaymentMainRun.java
@@ -0,0 +1,13 @@
+package com.luojun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMainRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMainRun.class,args);
+    }
+
+}
