7220af83a84b4c438e932c88619c64a24c1d005e
luojun <1195458334@qq.com>
2021-07-29 00:02:49 +0800

    EurekaClient-EurekaServer

--- cloud-consumer-order80/src/main/java/com/luojun/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/luojun/springcloud/OrderMain80.java
@@ -3,8 +3,11 @@ package com.luojun.springcloud;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
